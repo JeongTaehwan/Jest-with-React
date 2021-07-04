3be2338c35f295866b971a272d1cf5613f8fa650
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-07-04 17:51:19 +0900

    [ADD] snapshot 추가

--- src/components/Hello.test.js
+++ src/components/Hello.test.js
@@ -2,9 +2,23 @@ import { render, screen } from '@testing-library/react';
 import Hello from './Hello';
 
 const user = {
-    name: 'Taehwan',
+    name: 'Tom',
     age: 18,
 };
+
+const user2 = {
+    age: 20,
+};
+
+test("snapshot : name 있음", () => {
+    const el = render(<Hello user={user} />);
+    expect(el).toMatchSnapshot();
+});
+
+test("snapshot : name 없음", () => {
+    const el = render(<Hello user={user2} />);
+    expect(el).toMatchSnapshot();
+});
 
 test('"안녕하세요" 라는 글자가 포함됬는가?', () => {
     render(<Hello user={user} />); // render를 통해 Hello Components를 불러옴
